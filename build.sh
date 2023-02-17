47b20d5679bf7bf679868dda30fd2dc45d02fee1
Lubin <lgbxyz@gmail.com>
2023-02-17 17:16:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,16 +7,22 @@ cd `dirname $0`
 URL='http://download.savannah.gnu.org/releases/dmidecode/'
 FILENAME=$(curl -Lsk "${URL}" |grep -o 'dmidecode-.\+.tar.xz"' | sort -u | tail -n 1 | sed -e 's/"//' | tr -d '\n' | tr -d '\r')
 VERSION=$(echo "${FILENAME}"  | sed 's/.tar.xz//' | sed 's/dmidecode-//' | tr -d '\n' | tr -d '\r')
+LOCAL_VERSION=`cat version.txt`
+if [ "${VERSION}" == "${LOCAL_VERSION}" ] ; then
+  echo "Up to date"
+  exit 0
+fi
 
 ## downlaod source
 curl -Lsko ${FILENAME} ${URL}/${FILENAME}
 tar xf ${FILENAME}
+rm -f  ${FILENAME}
 
 
 ## build linux(amd64)
 docker run --rm -v `pwd`:/src -w /src -h dmidecode alpine:latest /src/linux_amd64.sh 
 
-## build windows(i686)
-docker run --rm -v `pwd`:/src -w /src -h dmidecode ubuntu:latest /src/win32_i686.sh
+## build windows(i686/x86_64)
+docker run --rm -v `pwd`:/src -w /src -h dmidecode ubuntu:latest /src/windows.sh
 
 

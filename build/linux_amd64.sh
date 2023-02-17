50d4e4d06544c2db7710358252813f2e0c6c6345
Lubin <lgbxyz@gmail.com>
2023-02-17 19:39:15 +0800

    Update linux_amd64.sh

--- build/linux_amd64.sh
+++ build/linux_amd64.sh
@@ -5,14 +5,9 @@ cd `dirname $0`
 apk update > /dev/null
 apk add build-base clang > /dev/null
 
-mkdir linux
 
 cd dmidecode-*
 make clean
 CC="clang" LDFLAGS="-static" make 
 make strip
-cp dmidecode biosdecode vpddecode ownership ../linux
-cd ..
-
-tar zcvf dmidecode_linux_amd64.tar.gz linux/*
-
+tar zcvf ../dmidecode_linux_amd64.tar.gz dmidecode biosdecode vpddecode ownership
